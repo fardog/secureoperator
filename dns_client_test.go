b21cb6b0f8767dce19b3ef91fe07b1dd041c2a3a
fardog <nate@milkandtang.com>
2017-04-12 08:16:47 -0700

    dns cache tests

--- dns_client_test.go
+++ dns_client_test.go
@@ -1,6 +1,10 @@
 package secureoperator
 
-import "testing"
+import (
+	"net"
+	"testing"
+	"time"
+)
 
 func TestParseEndpoint(t *testing.T) {
 	type Case struct {
@@ -41,5 +45,57 @@ func TestParseEndpointErrors(t *testing.T) {
 	_, err = ParseEndpoint("abc:53", 53)
 	if err != ErrFailedParsingIP {
 		t.Fatal("expected ErrFailedParsingIP")
+	}
+}
+
+func TestDNSCache(t *testing.T) {
+	d := newDNSCache()
+
+	_, ok := d.Get("wut")
+	if ok {
+		t.Error("expected to retrieve no record, but got one")
+	}
+
+	d.Set("wut", dnsCacheRecord{
+		msg:     nil,
+		ips:     []net.IP{net.ParseIP("8.8.8.8")},
+		expires: time.Now().Add(time.Minute * 5),
+	})
+
+	r, ok := d.Get("wut")
+	if !ok {
+		t.Fatal("expected to retrieve a record, but did not get one")
+	}
+
+	if len(r.ips) != 1 {
+		t.Fatalf("expected one IP, but got none")
+	}
+
+	if r.ips[0].String() != "8.8.8.8" {
+		t.Errorf("got unexpected IP: %v", r.ips[0].String())
+	}
+
+	d.Set("cool", dnsCacheRecord{
+		msg:     nil,
+		ips:     []net.IP{net.ParseIP("8.8.4.4")},
+		expires: time.Now().Add(time.Minute * 5),
+	})
+
+	r, ok = d.Get("cool")
+	if !ok {
+		t.Fatal("expected to retrieve a record, but did not get one")
+	}
+
+	if len(r.ips) != 1 {
+		t.Fatalf("expected one IP, but got none")
+	}
+
+	if r.ips[0].String() != "8.8.4.4" {
+		t.Errorf("got unexpected IP: %v", r.ips[0].String())
+	}
+
+	r, ok = d.Get("nope")
+	if ok {
+		t.Error("expected to retrieve no record, but got one")
 	}
 }
